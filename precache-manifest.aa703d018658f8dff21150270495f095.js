e31a019727f89f7b3650424713f4100c36c116da
Remeesha_Sajjad <98657530+Rameesha16@users.noreply.github.com>
2022-08-11 13:10:46 +0500

    Updates

---
+++ precache-manifest.aa703d018658f8dff21150270495f095.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82e92e6a2899c491ddf1cd06c2418cd1",
+    "url": "/Halalmeat-ecommorce-website/index.html"
+  },
+  {
+    "revision": "d721b8984c13d4dd0b9a",
+    "url": "/Halalmeat-ecommorce-website/static/css/2.157536d8.chunk.css"
+  },
+  {
+    "revision": "1b6b8f75051b0b665794",
+    "url": "/Halalmeat-ecommorce-website/static/css/main.214223b9.chunk.css"
+  },
+  {
+    "revision": "d721b8984c13d4dd0b9a",
+    "url": "/Halalmeat-ecommorce-website/static/js/2.59da90d0.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/Halalmeat-ecommorce-website/static/js/2.59da90d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b6b8f75051b0b665794",
+    "url": "/Halalmeat-ecommorce-website/static/js/main.8adbefa0.chunk.js"
+  },
+  {
+    "revision": "c1b358537fa5c99f6144",
+    "url": "/Halalmeat-ecommorce-website/static/js/runtime-main.7415f652.js"
+  },
+  {
+    "revision": "419b45388b591cb76de2a07891df90f2",
+    "url": "/Halalmeat-ecommorce-website/static/media/notfound.419b4538.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/Halalmeat-ecommorce-website/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/Halalmeat-ecommorce-website/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/Halalmeat-ecommorce-website/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/Halalmeat-ecommorce-website/static/media/slick.f97e3bbf.svg"
+  }
+]);
